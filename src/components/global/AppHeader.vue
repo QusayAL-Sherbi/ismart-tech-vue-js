dfbeff3285f7ee7feec08d34172f1f3678bcf0cf
QusayAL-Sherbi <qusay.alsherbi@gmail.com>
2021-04-11 13:20:13 +0300

    Finish Creating Project Files Structure

---
+++ src/components/global/AppHeader.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+header
+  h1 Header Component
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ]
+}
